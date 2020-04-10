4b7d0be2988efbc1ae243b4040012fb6750e8237
Steve Fentriss <sfentriss2020@gmail.com>
2020-04-10 17:09:53 +0000

    add heat map and include heat map in home page

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, send_from_directory, render_template
+import gmaps
+import gmaps.datasets
 
 app = Flask(__name__, static_url_path='')
 
@@ -36,6 +38,22 @@ def resources():
 @app.route("/stats")
 def stats():
 	return render_template('stats.html', name="BOB", title="Stats - vaccess")
+
+@app.route('/gmappage')
+def gmappage():
+	# gmaps.configure(api_key="AIzaSyB82meUu-KE5JSvX7oUvALGybDAebeAak0")
+	# locations = gmaps.datasets.load_dataset({
+ #       "url": "https://s3-eu-west-1.amazonaws.com/jupyter-gmaps-examples/taxi_data.csv",  # noqa
+ #       "description": "Taxi pickup location data in San Francisco",
+ #       "headers": ["latitude", "longitude"],
+ #       "types": [float, float]
+ #   })
+	# new_york_coordinates = (40.75, -74.00)
+	# fig = gmaps.figure(center=new_york_coordinates, zoom_level=12)
+	# fig.add_layer(gmaps.heatmap_layer(locations))
+	# fig
+	# return "I have maps.";
+		return render_template('gmap.html')
 
 
 
