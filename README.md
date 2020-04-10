541eccf1879ec100978568d2a97e2b4c67af4b6a
Steve Fentriss <sfentriss2020@gmail.com>
2020-04-10 02:40:58 +0000

    add front end code, now ready to break this app into a few pages

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 VACCESS is a data-driven web application which informs pharmaceutical production and distribution of vaccines using a proprietary combination of historic and real-time population health data.
 
 To know more , visit : www.vaccess.io
+
+
+# To run the app
+    $ export FLASK_APP=hello.py
+    $ flask run
+    * Running on http://127.0.0.1:5000/
+
+# To run the app on an externally visible server use
+    $ flask run --host=0.0.0.0
+
--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, request, send_from_directory, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
+
+
+#
+#
+# This is temporary, slow code to serve static files.
+# In production would prefer nginx or apache handle this.
+#
+@app.route('/assets/<path:path>')
+def send_assets(path):
+    return send_from_directory('/opt/www/VACCESS/static/assets', path)
+@app.route('/bootstrap/<path:path>')
+def send_bootstrap(path):
+    return send_from_directory('/opt/www/VACCESS/static/bootstrap', path)
+@app.route('/plugins/<path:path>')
+def send_plugins(path):
+    return send_from_directory('/opt/www/VACCESS/static/plugins', path)
+#
+#
+#
 
 @app.route("/")
 def index():
-	return "Hello World"
+	return render_template('index.html', name="BOB")
 
 if __name__ == "__main__":
 	app.run(debug = True)
